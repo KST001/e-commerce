2085749aff2b1675093b6423d55f1276e2fe5b22
khemrajsinght <khemrajsinght57@gmail.comm>
2023-08-23 02:13:45 +0545

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import Home from "./Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Error from "./pages/Error";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h1>E-Commerce Website</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/id:" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
